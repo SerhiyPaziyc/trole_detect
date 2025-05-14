2b2e5fdf0d95ca8c3b0b94e07a5d021ce6c4a081
SerhiyPaziyc <serhiypaziyc15@gmail.com>
2025-05-14 20:07:34 +0300

    test2

--- web/app.py
+++ web/app.py
@@ -1,4 +1,7 @@
 # web/app.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask import Flask, render_template, request
 from app.hate_detector import HateDetector
